b7751594c04ce54846e67bbfce26a322437c1796
tbaldeviso <tbbaldeviso@ucdavis.edu>
2019-11-21 18:58:23 +0000

    Pushed initial files

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	gcc -o maxTweeter maxTweeter.c
+clean:
+	rm -f maxTweeter
---
+++ maxTweeter.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv) {
+	FILE* stream = fopen(argv[1], "r");
+	return 0;
+}
