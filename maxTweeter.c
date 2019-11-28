842141d3594af39303e2e548872878d787628314
rzahmed <58150145+rzahmed@users.noreply.github.com>
2019-11-28 13:45:34 -0800

    Update maxTweeter.c
    So I was working on this and I realized strtok does not work when there are empty fields. So depending on the files they test with, if there are empty fields, we will have to write our own strtok. 
    
    Besides that :
    1. If there's no header, the file is invalid.
    2. There must be just one name column.  It could be either name or "name" depending on if it's quoted.
    3. This means that all items in a column may be quoted or not.  If the header is quoted, the whole column should be.  If the header isn't quoted, the whole column won't be either.
    4, include "\n", the maximum is 1024, otherwise it's invalid input (the whole file become invalid)
        4.1 Is this saying, if we found one line is exceed 1024 length, then we just report "Invalid Input Format".
    5, Once the lines exceed 20000, report "Invalid Input Format".
    6, Once we found a additional comma, report "Invalid Input Format".

--- maxTweeter.c
+++ maxTweeter.c
@@ -2,7 +2,63 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char **argv) {
-	FILE* stream = fopen(argv[1], "r");
+#define file_length 20000
+#define line_length 1024
+
+int get_namepos(char* filename){ //gets positon of the name column
+    char line[line_length];
+    char *token;
+    int name_pos;
+    FILE* file = fopen(filename, "r");
+
+    fgets(line, line_length, file);
+    token = strtok(line, ",");
+    name_pos = 0;
+    while (token != NULL){
+        if ((strcmp(token, "name") == 0 || strcmp(token, "\"name\"") == 0))
+            return name_pos;
+        token = strtok(NULL, ",");
+        name_pos++;
+    }
+    fclose(file);
+    return 0;
+}
+
+int main(int argc, char **argv) { //for now puts the names in a array of strings
+	`FILE* stream = fopen(argv[1], "r");
+
+    char **names = malloc(file_length * sizeof(char *));
+    int i;
+    for (i = 0; i < file_length; ++i) {
+        names[i] = (char *)malloc(line_length+1);
+    }
+
+    char line[line_length];
+    char *token;
+    int counter = 0;
+    int name_pos = get_namepos(argv[1]);
+    while(fgets(line, sizeof(line), stream)){
+        token = strtok(line, ",");
+        int j = 0;
+        while(token != NULL){
+            if (j == name_pos){
+                break;
+            }
+            token = strtok(NULL, ",");
+            j++;
+        }
+        strcpy(names[counter++], token);
+    }
+
+    for (i = 0; i < 20; ++i) {
+        printf("%s\n", names[i]);
+    }
+
+
+    for (i = 0; i < file_length; ++i) {
+        free(names[i]);
+    }
+    free(names);
+
 	return 0;
 }
