2d86789cb5804117d09ce1b28421f60d8c2ab02b
rzahmed <58150145+rzahmed@users.noreply.github.com>
2019-11-29 12:11:05 -0800

    Update maxTweeter.c
    Okay, I changed strtok to strsep which accounts for empty fields. Right now, it is able to get all the names for the csv file from previous hw assignment (around 11330 names).

--- maxTweeter.c
+++ maxTweeter.c
@@ -5,28 +5,29 @@
 #define file_length 20000
 #define line_length 1024
 
-int get_namepos(char* filename){ //gets positon of the name column
+int get_namepos(char* file_path){   //gets positon of the name column
     char line[line_length];
     char *token;
+    char *string;
     int name_pos;
-    FILE* file = fopen(filename, "r");
+    FILE* file = fopen(file_path, "r");
 
     fgets(line, line_length, file);
-    token = strtok(line, ",");
+    string = strdup(line);
+    token = strsep(&string, ",");
     name_pos = 0;
     while (token != NULL){
-        if ((strcmp(token, "name") == 0 || strcmp(token, "\"name\"") == 0))
+        if (strcmp(token, "name") == 0 )
             return name_pos;
-        token = strtok(NULL, ",");
+        token = strsep(&string, ",");
         name_pos++;
     }
     fclose(file);
     return 0;
 }
 
-int main(int argc, char **argv) { //for now puts the names in a array of strings
-	`FILE* stream = fopen(argv[1], "r");
-
+char** get_names(char* file_path){
+    FILE* file = fopen(file_path, "r");
     char **names = malloc(file_length * sizeof(char *));
     int i;
     for (i = 0; i < file_length; ++i) {
@@ -35,25 +36,35 @@ int main(int argc, char **argv) { //for now puts the names in a array of strings
 
     char line[line_length];
     char *token;
+    char *string;
     int counter = 0;
-    int name_pos = get_namepos(argv[1]);
-    while(fgets(line, sizeof(line), stream)){
-        token = strtok(line, ",");
+    int name_pos = get_namepos(file_path);
+    fgets(line, sizeof(line), file);    //skip header
+    while(fgets(line, sizeof(line), file)){
+        string = strdup(line);
+        token = strsep(&string, ",");
         int j = 0;
         while(token != NULL){
             if (j == name_pos){
                 break;
             }
-            token = strtok(NULL, ",");
+            token = strsep(&string, ",");
             j++;
         }
         strcpy(names[counter++], token);
     }
+    fclose(file);
+    return names;
+}
 
-    for (i = 0; i < 20; ++i) {
+int main(int argc, char **argv) {   //for now puts the names in a array of strings
+    char **names;
+    names = get_names(argv[1]);
+
+    int i;
+    for (i = 0; i < 11329; ++i) {
         printf("%s\n", names[i]);
     }
-
 
     for (i = 0; i < file_length; ++i) {
         free(names[i]);
